d2e95bfb1d6fac53b61d1e2b0ad17a8be713a8a1
TanyaFox <tanya-smile1994@mail.ru>
2016-02-29 13:06:38 +0400

    Adding folders

--- PuzzleGame/Views/MainWindowView.xaml.cs
+++ PuzzleGame/Views/MainWindowView.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace PuzzleGame
+namespace PuzzleGame.Views
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для MainWindowView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindowView : Window
     {
-        public MainWindow()
+        public MainWindowView()
         {
             InitializeComponent();
         }
