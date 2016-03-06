ace1775f7c72926ac2497bc2282f3f932d30d257
Igor <ivryazanov@edu.hse.ru>
2016-03-06 15:01:49 +0300

    Cell+Field

--- PuzzleGame/Models/Cell.cs
+++ PuzzleGame/Models/Cell.cs
@@ -8,6 +8,24 @@ namespace PuzzleGame.Models
 {
     class Cell
     {
+        public bool IsCorrect { get; set; }
+        public int CurrentElement { get; set; }
+        public int Count { get; set; }
 
+        public Cell(int current, int count)
+        {
+            if (current == count)
+                IsCorrect = true;
+            else
+                IsCorrect = false;
+            this.Count = count;
+            this.CurrentElement = current;
+        }
+        public Cell(int count)
+        {
+            IsCorrect = false;
+            this.Count = count;
+            this.CurrentElement = 1;
+        }
     }
 }
---
+++ PuzzleGame/Models/Field.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuzzleGame.Models
+{
+    class Field
+    {
+        public List<Cell> ListCell { get; set; }
+
+        public Field(int cells)
+        {
+            List<Cell> Templist = new List<Cell>();
+            for (int i = 1; i <= cells; i++)
+            {
+                Templist.Add(new Cell(i));
+            }
+            this.ListCell = Templist;
+        }
+
+        public Field(List<int> rnd)
+        {
+            List<Cell> Templist = new List<Cell>();
+            for (int i = 1; i <= rnd.Count; i++)
+            {
+                Templist.Add(new Cell(rnd[i], i));
+            }
+        }
+    }
+}
