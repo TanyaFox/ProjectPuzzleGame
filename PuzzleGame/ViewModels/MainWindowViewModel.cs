2be8e51d022e1eb31b8a98febcd7541edf020007
TanyaFox <tanya-smile1994@mail.ru>
2016-03-10 18:30:21 +0400

    MainWindow and NewGameWindow are linked

--- PuzzleGame/ViewModels/MainWindowViewModel.cs
+++ PuzzleGame/ViewModels/MainWindowViewModel.cs
@@ -3,12 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using PuzzleGame.Views;
 
 namespace PuzzleGame.ViewModels
 {
     class MainWindowViewModel
     {
         public MainWindowViewModel()
+        {
+            ButtonNewGameCommand = new Command(arg => ButtonNewGameClick());
+        }
+
+        public ICommand ButtonNewGameCommand { get; set; }
+
+        private void ButtonNewGameClick()
+        {
+            var newGameWindow = new NewGameWindowView();
+            newGameWindow.ShowDialog();
+        }
+
+        public ICommand ButtonSavedGameCommand { get; set; }
+
+        private void ButtonSavedGameClick()
+        {
+
+        }
+
+        public ICommand ButtonAuthorsCommand { get; set; }
+
+        private void ButtonAuthorsClick()
+        {
+
+        }
+
+        public ICommand ButtonExitCommand { get; set; }
+
+        private void ButtonExitClick()
         {
 
         }
--- PuzzleGame/Views/NewGameWindowView.xaml.cs
+++ PuzzleGame/Views/NewGameWindowView.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using PuzzleGame.ViewModels;
 
 namespace PuzzleGame.Views
 {
@@ -22,6 +23,10 @@ namespace PuzzleGame.Views
         public NewGameWindowView()
         {
             InitializeComponent();
+
+            NewGameWindowViewModel vm = new NewGameWindowViewModel();
+            this.DataContext = vm;
+
         }
     }
 }
