6eb57c89b54e01c89da546d74c028d8ca81d89a0
Софья <sonia.p@inbox.ru>
2016-03-17 23:33:47 +0300

    Поменяла имя на id

--- PuzzleGame/DataBase/DataBase.cs
+++ PuzzleGame/DataBase/DataBase.cs
@@ -20,7 +20,7 @@ namespace PuzzleGame
             using (SqlConnection connection = new SqlConnection(conString))
             {
                 connection.Open();
-                using (SqlCommand cmd = new SqlCommand("SELECT p.Name, p.Miniature FROM dbo.Picture as p", connection))//список миниатюр
+                using (SqlCommand cmd = new SqlCommand("SELECT p.Id, p.Name, p.Miniature FROM dbo.Picture as p", connection))//список миниатюр
                 {
                     using (SqlDataReader reader = cmd.ExecuteReader())
                     {
@@ -39,7 +39,7 @@ namespace PuzzleGame
         }
 
 
-        public Dictionary<string, MemoryStream> LoadPuzzle(string PictureName, int GameLevel)//Метод возвращает словарь типа "картинка-положение картинки"
+        public Dictionary<string, MemoryStream> LoadPuzzle(int ImageId, int GameLevel)//Метод возвращает словарь типа "картинка-положение картинки"
         {
             Dictionary<string, MemoryStream> PuzzleParts = new Dictionary<string, MemoryStream>();
             using (SqlConnection connection = new SqlConnection(conString))
@@ -47,7 +47,7 @@ namespace PuzzleGame
 
                 var cmd = new SqlCommand("ЗагрузкаПазла", connection);
                 cmd.CommandType = CommandType.StoredProcedure;
-                cmd.Parameters.AddWithValue("@Name", PictureName);
+                cmd.Parameters.AddWithValue("@Id", ImageId);
                 cmd.Parameters.AddWithValue("@Level", GameLevel);
                 connection.Open();
                 using (SqlDataReader reader = cmd.ExecuteReader())
@@ -70,14 +70,14 @@ namespace PuzzleGame
             return PuzzleParts;
         }
 
-        public void SaveGame(string ImageName, int Type, int Difficulty, string PartLocation)
+        public void SaveGame(int ImageId, int Type, int Difficulty, string PartLocation)
         {
             using (SqlConnection connection = new SqlConnection(conString))
             {
 
                 var cmd = new SqlCommand("СохранениеИгры", connection);
                 cmd.CommandType = CommandType.StoredProcedure;
-                cmd.Parameters.AddWithValue("@ImageName", ImageName);
+                cmd.Parameters.AddWithValue("@ImageName", ImageId);
                 cmd.Parameters.AddWithValue("@Mode", Type);
                 cmd.Parameters.AddWithValue("@Level", Difficulty);
                 cmd.Parameters.AddWithValue("@Location", PartLocation);
@@ -106,14 +106,14 @@ namespace PuzzleGame
             }
         }
 
-        public void AddPartsOfPicture(string ImageName, byte[] imageBytes, int Difficulty, int PartLocation)
+        public void AddPartsOfPicture(int ImageId, byte[] imageBytes, int Difficulty, int PartLocation)
         {
             using (SqlConnection connection = new SqlConnection(conString))
             {
 
                 var cmd = new SqlCommand("ДобавлениеЧасти", connection);
                 cmd.CommandType = CommandType.StoredProcedure;
-                cmd.Parameters.AddWithValue("@Name", ImageName);
+                cmd.Parameters.AddWithValue("@ImageId", ImageId);
                 cmd.Parameters.AddWithValue("@Part", imageBytes);
                 cmd.Parameters.AddWithValue("@Level", Difficulty);
                 cmd.Parameters.AddWithValue("@Location", PartLocation);
@@ -142,6 +142,11 @@ namespace PuzzleGame
             }
             return LastGame;
         }
+    }
+
+    class Miniature
+    {
+       
     }
 }
 
